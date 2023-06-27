204cebf1bd039c49bb177bb19c530129a4d37ff8
Aleksander Dreszler <olek.d93@gmail.com>
2023-06-27 23:56:43 +0200

    Stworzono klasę reprezentującą email

---
+++ src/main/java/pl/adreszler/contactpage/Email.java
@@ -0,0 +1,44 @@
+package pl.adreszler.contactpage;
+
+class Email {
+
+    private String sender;
+    private String recipient;
+    private String subject;
+    private String content;
+
+    public Email() {
+    }
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    public String getRecipient() {
+        return recipient;
+    }
+
+    public void setRecipient(String recipient) {
+        this.recipient = recipient;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
