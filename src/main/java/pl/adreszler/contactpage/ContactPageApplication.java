008dfdd16b16628a58561354a810cdafec2f6dec
Aleksander Dreszler <olek.d93@gmail.com>
2023-06-27 20:26:48 +0200

    Utworzono projekt

---
+++ src/main/java/pl/adreszler/contactpage/ContactPageApplication.java
@@ -0,0 +1,13 @@
+package pl.adreszler.contactpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactPageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactPageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/adreszler/contactpage/ContactPageApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.adreszler.contactpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactPageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
